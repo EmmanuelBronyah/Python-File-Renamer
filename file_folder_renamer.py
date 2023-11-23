5d583ba233e436f28b39d3eeee4ffd726bbdac01
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2023-11-23 02:05:59 -0800

    Added all necessary functionalities.

---
+++ file_folder_renamer.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+
+def main():
+    """
+    Executes the entire program.
+
+    :return: None.
+    """
+    right_path = True
+    while right_path:
+        path = get_path()
+        if path != -1 and path is not None:
+            do_renaming(path)
+        else:
+            if path == -1:
+                print('Program ended.')
+                right_path = False
+
+
+def get_path():
+    user_folders = ['Desktop', 'Documents', 'Downloads', 'Music', 'Pictures', 'Videos']
+
+    path = input(r'Enter the path to the directory where the files/folders you want to rename reside: ') or 'exit'
+
+    user_folder_in_path = [user_folder for user_folder in user_folders if user_folder.lower() == path.lower()]
+    user_folder_in_path = user_folder_in_path[0] if user_folder_in_path != [] else None
+
+    if path == 'exit':
+        return -1
+    if any([os.path.isfile(path), os.path.isdir(path), user_folder_in_path]):
+        if user_folder_in_path:
+            path = os.path.join(os.path.expanduser('~'), user_folder_in_path)
+            return path
+        else:
+            return path
+    else:
+        print('Invalid path. The destination path must me a file or folder.')
+
+
+def do_renaming(path):
+    for obj in os.listdir(path):
+        obj_split = re.split(r'[,.+|\s\\%$*_#@~-]+', obj)
+        new_obj_name = '-'.join(obj_split).lower()
+        os.rename(os.path.join(path, obj), os.path.join(path, new_obj_name))
+    print('Files/folders renamed.')
+
+
+if __name__ == '__main__':
+    main()
